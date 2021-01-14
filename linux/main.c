61949b7efe86c76d687f54e8632310e9e33a7472
Danny Steinbrecher <46423967+darthkali@users.noreply.github.com>
2021-01-14 14:24:56 +0100

    change

--- linux/main.c
+++ linux/main.c
@@ -12,105 +12,100 @@
 //#define SERVER "www.schorcht.net"
 
 int main(int argc, const char* argv[]) {
-	printf("Starting script.\n");
-	int tcp = 0;
+    printf("Starting script.\n");
+    int tcp = 0;
 
     if(tcp != 0 && tcp != 1){
         perror("no valid protocol! use either tcp or udp");
         return -1;
     }
 
-	int sd;
-	struct sockaddr_in saddr;
-	socklen_t saddrlen;
-	char sbuf[BUFFERSIZE];
-	char rbuf[BUFFERSIZE];
-	int sbuflen;
-	int rbuflen;
+    int sd;
+    struct sockaddr_in saddr;
+    socklen_t saddrlen;
+    char sbuf[BUFFERSIZE];
+    char rbuf[BUFFERSIZE];
+    int sbuflen;
+    int rbuflen;
 
-	strcpy(sbuf, "Danny Steinbrecher");
+    strcpy(sbuf, "Danny Steinbrecher");
 
     bzero ( ( char * ) &saddr, sizeof ( saddr ) );
     saddr.sin_family = AF_INET;
     saddr.sin_addr.s_addr = inet_addr ( SERVER );
-    saddr.sin_port = htons (PORT_TCP);
 
     sbuflen = strlen(sbuf);
     saddrlen = sizeof(saddr);
 
+    if ((sd = socket(PF_INET, SOCK_STREAM, 0)) < 0) {
+        perror("socket");
+        return -1;
+    }
 
+    if (tcp == 0) { 		// -------------------- UDP
+        printf("UDP.\n");
 
-	if (tcp == 0) { 		// -------------------- UDP
-		printf("UDP.\n");
-
+        saddr.sin_port = htons (PORT_UDP);
 
         if ((sd = socket(PF_INET, SOCK_DGRAM, 0)) < 0) {
             perror("socket");
             return -1;
         }
 
-		// Send my name
-        printf("SendTo:\n");
+        // Send my name
         if (sendto(sd, sbuf, sbuflen, 0, (struct sockaddr*)&saddr, saddrlen) != sbuflen) {
             perror("sendto");
             return -1;
         }
 
-
-
-		// receive response
-        printf("receive response:\n");
-		if ((rbuflen = recvfrom(sd, rbuf, BUFFERSIZE, 0, (struct sockaddr*)&saddr, &saddrlen)) < 0) {
-			perror("recvfrom");
-			return -1;
-		}
-
-		rbuf[rbuflen] = 0;
-		printf("%s\n", rbuf);
-
-		// echo response		
-		if (sendto(sd, rbuf, rbuflen, 0, (struct sockaddr*)&saddr, saddrlen) != rbuflen) {
-			perror("sendto");
-			return -1;
-		}
-        close(sd);
-
-	} else {	// -------------------- TCP
-		printf("TCP.\n");
-
-        if ((sd = socket(PF_INET, SOCK_STREAM, 0)) < 0) {
-            perror("socket");
+        // receive response
+        if ((rbuflen = recvfrom(sd, rbuf, BUFFERSIZE, 0, (struct sockaddr*)&saddr, &saddrlen)) < 0) {
+            perror("recvfrom");
             return -1;
         }
 
-		// Send my name
-		if (connect(sd, (struct sockaddr*)&saddr, saddrlen) < 0) {
-			perror("connect");
-			return -1;
-		}
+        rbuf[rbuflen] = 0;
+        printf("%s\n", rbuf);
 
-		if (send(sd, sbuf, sbuflen, 0) != sbuflen) {
-			perror("send");
-			return -1;
-		}
-
-		// receive response
-		if ((rbuflen = recv(sd, rbuf, BUFFERSIZE, 0)) < 0) {
-			perror("recvfrom");
-			return -1;
-		}
-
-		rbuf[rbuflen] = 0;
-		printf("%s\n", rbuf);
-
-		// echo response
-		if (send(sd, rbuf, rbuflen, 0) != rbuflen) {
-			perror("send");
-			return -1;
-		}
+        // echo response
+        if (sendto(sd, rbuf, rbuflen, 0, (struct sockaddr*)&saddr, saddrlen) != rbuflen) {
+            perror("sendto");
+            return -1;
+        }
         close(sd);
-	}
 
-	return 0;
+    } else {	// -------------------- TCP
+        printf("TCP.\n");
+
+        saddr.sin_port = htons (PORT_TCP);
+
+        // Send my name
+        if (connect(sd, (struct sockaddr*)&saddr, saddrlen) < 0) {
+            perror("connect");
+            return -1;
+        }
+
+        if (send(sd, sbuf, sbuflen, 0) != sbuflen) {
+            perror("send");
+            return -1;
+        }
+
+        // receive response
+        if ((rbuflen = recv(sd, rbuf, BUFFERSIZE, 0)) < 0) {
+            perror("recvfrom");
+            return -1;
+        }
+
+        rbuf[rbuflen] = 0;
+        printf("%s\n", rbuf);
+
+        // echo response
+        if (send(sd, rbuf, rbuflen, 0) != rbuflen) {
+            perror("send");
+            return -1;
+        }
+        close(sd);
+    }
+
+    return 0;
 }
-
